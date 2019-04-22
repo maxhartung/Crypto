4b4253c73a34f15206d38f37e634816f9a7a30b3
Max Hartung <maxhartung97@outlook.com>
2019-04-23 00:01:14 +0300

    Fixed

--- Crypto/main.cpp
+++ Crypto/main.cpp
@@ -1,33 +1,74 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
-string encrypt_Caeser(string text, int s){
-    string result = "";
-    
-    for (int i=0;i<text.length();i++)
-    {
-        if (isupper(text[i]))
-            result += char(int(text[i]+s-65)%26 +65);
-        else
-            result += char(int(text[i]+s-97)%26 +97);
-    }
-    return result;
+string path;
+
+std::string getOsName()
+{
+#ifdef _WIN32
+    return "Windows 32-bit";
+#elif _WIN64
+    return "Windows 64-bit";
+#elif __unix || __unix__
+    return "Unix";
+#elif __APPLE__ || __MACH__
+    return "Mac OSX";
+#elif __linux__
+    return "Linux";
+#elif __FreeBSD__
+    return "FreeBSD";
+#else
+    return "Other";
+#endif
 }
 
-int main(int argc, const char * argv[]) {
+void save_File(string encrypted_string){
+    string encrypted_file_path;
     
-    string path;
+    if (getOsName() == "Windows 64-bit" || getOsName() == "Windows 32-bit"){
+        encrypted_file_path = "C:\encrypted_file.txt";
+    }
+    else {
+        encrypted_file_path = "/Users/max/encrypted_file.txt";
+    }
+    
+    std::ofstream encrypted_file(encrypted_file_path);
+    encrypted_file << encrypted_string << std::endl;
+    encrypted_file.close();
+    
+}
+
+class Encrypt{
+public:
+        string caeser_Method(string text, int s){
+            string result = "";
+            stringstream buffer;
+            buffer << ifstream(path).rdbuf();
+            buffer.str();
+            
+            for (int i=0;i<buffer.str().length();i++)
+            {
+                if (isupper(buffer.str()[i]))
+                    result += char(int(buffer.str()[i]+s-65)%26 +65);
+                else
+                    result += char(int(buffer.str()[i]+s-97)%26 +97);
+            }
+            save_File(result);
+            return "Fisierul a fost scris cu succes !";
+        }
+    
+        string metoda_lui_peste_prajit(string text){
+        //TODO
+        return 0;
+    }
+    
+};
+
+void show_Options(){
     char option;
-    
-    if (argc == 2){
-        cout << "Calea fisierului este: " << "'" << argv[1] << "'" << endl << endl << endl;
-        path = argv[1];
-    }
-    else{
-        cout << "Introduceti calea fisierului pentru criptare: " << endl << endl;
-        cin >> path;
-        cout << path;
-    }
+
     cout << "Alegeti metoda de criptare: " << endl << endl;
     
     cout << "1. Metoda la ala francez" << endl;
@@ -35,10 +76,16 @@ int main(int argc, const char * argv[]) {
     cout << "3. Metoda la YYYYYYYYYYY" << endl << endl;
     
     cin >> option;
-   // string stringfe = "TEST";
+    // string stringfe = "TEST";
+    // path = "";
+    Encrypt e;
     switch (option) {
         case '1':
-            cout << encrypt_Caeser(path,5) << endl << endl;
+            if (getOsName() == "Windows 64-bit" || getOsName() == "Windows 32-bit"){
+                system("cls"); // Golim ecranul pentru Windows, pentru Mac inca caut o solutie...
+            }
+            cout << endl;
+            cout << e.caeser_Method(path, 5) << endl << endl;
             break;
             
         case '2':
@@ -48,7 +95,23 @@ int main(int argc, const char * argv[]) {
             //TODO
             break;
     }
-     return 0;
+}
+
+int main(int argc, const char * argv[]) {
+
+    if (argc == 2){
+        cout << "Calea fisierului este: " << "'" << argv[1] << "'" << endl << endl << endl;
+        path = argv[1];
+    }
+    else{
+        cout << "Introduceti calea fisierului pentru criptare: " << endl << endl;
+        cin >> path;
+        cout << path;
+    }
+    while(1){
+     show_Options();
+    }
+    return 0;
 }
 
 // Ma gandeam sa facem mai inteligent programul aka cand scrii din linia de comanda: "crypto.exe cale_fisier" sa-l preia automat si daca se executa fara cale, sa te intrebe de calea fisierului, e functional by the way
